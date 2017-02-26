ab64e891a662b4cc10544d7003c130a7b9c8678c
Anikin Artyom <anikinartyom@gmail.com>
2017-02-26 23:58:54 +0500

    Update session_id field

--- db/migrate/20170226114934_create_rooms.rb
+++ db/migrate/20170226114934_create_rooms.rb
@@ -2,7 +2,7 @@ class CreateRooms < ActiveRecord::Migration[5.0]
   def change
     create_table :rooms do |t|
       t.string :name
-      t.integer :session_id
+      t.string :session_id
       t.boolean :public
 
       t.timestamps
