00e9aab63e1a20bf3d1e2462eb2cf6cfe8a09a34
Anikin Artyom <anikinartyom@gmail.com>
2017-02-27 09:19:44 +0500

    Test for Message model

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:session_user) }
+  it { should belong_to(:room) }
 end
