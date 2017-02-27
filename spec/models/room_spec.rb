736e648747452b3f607d8d97fd0b0847605842ff
Anikin Artyom <anikinartyom@gmail.com>
2017-02-27 09:23:51 +0500

    Test for Room model

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+  it { should have_many(:session_users) }
+  it { should have_many(:users).through(:session_users) }
+  it { should have_many(:messages) }
+
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:session_id) }
 end
