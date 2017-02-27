3cc7cd2347b24db0dfb5ea6bb1589df812b6c33b
Anikin Artyom <aanikin@naumen.ru>
2017-02-27 11:48:02 +0500

    Test for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:session_users) }
+  it { should have_many(:rooms).through(:session_users) }
+
+  describe "#has_session?" do
+    let(:user) { create(:user) }
+    let(:room) { create(:room) } 
+
+    it "has session" do
+      create(:session_user, room: room, user: user)
+      expect(user.has_session?(room.id)).to be_truthy
+    end
+
+    it "has not session" do
+      expect(user.has_session?(room.id)).to be_falsey
+    end
+  end
+
+  describe "#session" do
+    let(:user) { create(:user) }
+    let(:room) { create(:room) } 
+
+    it "get user session for the room" do
+      session_user = create(:session_user, room: room, user: user)
+      expect(user.session(room.id)).to eq(session_user)
+    end
+  end
 end
